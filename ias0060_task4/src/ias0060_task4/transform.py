a8587a94c2ee2129ef37927e3a79a82554777032
nevermore <miloom@ttu.ee>
2023-03-03 11:57:57 +0200

    New structure and corrections

--- ias0060_task4/src/ias0060_task4/transform.py
+++ ias0060_task4/src/ias0060_task4/transform.py
@@ -6,49 +6,44 @@ def world_to_grid(x, y, origin_x, origin_y, width, height, resolution):
     # check boundaries
     x_boundary = int(width / resolution)
     y_boundary = int(height / resolution)
-    if (grid_x <= x_boundary and grid_y <= y_boundary and x >= origin_x and y >= origin_y):
-        return (grid_x, grid_y)
+    if grid_x <= x_boundary and grid_y <= y_boundary and x >= origin_x and y >= origin_y:
+        return grid_x, grid_y
     else:
         return None
 
 
 def grid_to_world(gx, gy, origin_x, origin_y, width, height, resolution):
-    return (gx * resolution + origin_x, gy * resolution + origin_y)
-    #todo kontrollida kas gx gy on kaardilt valjas
-    #kontrollida punkti keskele lisamist 
+    return gx * resolution + origin_x + (resolution / 2) * pow(-1, gx < 0),\
+        gy * resolution + origin_y + (resolution / 2) * pow(-1, gy < 0)
 
-def grid_to_world_2(gx,gy,origin_x,origin_y,width,height,resolution): #TODO!!!!! NOT SURE IT IS CORRECT ATM!!!!!!
+
+# TODO!!!!! NOT SURE IT IS CORRECT ATM!!!!!!
+def grid_to_world_2(gx, gy, origin_x, origin_y, width, height, resolution):
     return None
 
+
 def print_world_info(origin_x, origin_y, width, height, resolution):
-    print(f"Grid size: {int(width / resolution)}x{int(height / resolution)} cells, each {resolution}x{resolution} meters")
+    print(
+        f"Grid size: {int(width / resolution)}x{int(height / resolution)} cells, each {resolution}x{resolution} meters")
     print(f"Grid origin: ({origin_x}, {origin_y})")
     print(f"Grid boundaries: ({origin_x}, {origin_y}) - ({origin_x + width}, {origin_y + height})")
 
-def test_grid_to_world():
-    origin_x = 0
-    origin_y = 0
-    width = 100
-    height = 100
-    resolution = 10
+
+def test_grid_to_world(origin_x=0, origin_y=0, width=100, height=100, resolution=10):
     print_world_info(origin_x, origin_y, width, height, resolution)
     # Test the function with some example points
-    points = [(1, 1), (5, 7), (-5, 0), (10, 15)]
+    points = [(1, 1), (5, 7), (-5, 0), (10, 15), (0, 0)]
     for point in points:
         world_cords = grid_to_world(point[0], point[1], origin_x, origin_y, width, height, resolution)
         print(f"Point {point} is in world coords {world_cords}")
 
     print(f"Input: Start {(origin_x, origin_y)}, Max {(origin_x + width, origin_y + height)}, Resolution {resolution}")
 
-if __name__ == '__main__':
-    origin_x = 0
-    origin_y = 0
-    width = 100  # 10 kuubikut
-    height = 100
-    resolution = 10
+
+def test_world_to_grid(origin_x=0, origin_y=0, width=100, height=100, resolution=10):
     print_world_info(origin_x, origin_y, width, height, resolution)
     # Test the function with some example points
-    points = [(10, 10), (55, 70), (-5, 20), (120, 80)]
+    points = [(10, 10), (55, 70), (-5, 20), (120, 80), (0, 0)]
     for point in points:
         grid_coords = world_to_grid(point[0], point[1], origin_x, origin_y, width, height, resolution)
         if grid_coords:
@@ -56,8 +51,7 @@ if __name__ == '__main__':
         else:
             print(f"World to Grid: Point {point} is outside the grid")
 
-            
+
+if __name__ == '__main__':
+    test_world_to_grid()
     test_grid_to_world()
-
-
-    
