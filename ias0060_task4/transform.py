584ca165be7432be7ba4e497c568b53902d00c18
Eva Vesterblom <evgenia.udod@ttu.ee>
2023-03-03 11:24:59 +0200

    init grid_to_world

--- ias0060_task4/transform.py
+++ ias0060_task4/transform.py
@@ -2,7 +2,6 @@ def world_to_grid(x,y,origin_x,origin_y,width,height,resolution):
     #grid where x,y falls
     grid_x = int((x - origin_x )/ resolution)
     grid_y = int((y - origin_y) / resolution)
-    print(grid_x, grid_y)
 
     #check boundaries
     x_boundary = width/resolution
@@ -14,8 +13,23 @@ def world_to_grid(x,y,origin_x,origin_y,width,height,resolution):
 
 
 
-def grid_to_world(gx,gy,origin_x,origin_y,width,height,resolution):
-    return None
+def grid_to_world(gx,gy,origin_x,origin_y,width,height,resolution): #TODO!!!!! NOT SURE IT IS CORRECT ATM!!!!!!
+    x_boundary = origin_x + width
+    y_boundary = origin_y + height
+    if (gx == x_boundary and gy == y_boundary):
+        return (x_boundary, y_boundary)
+    elif (gx > origin_x and gy > origin_y and gx <= x_boundary and gy <= y_boundary):
+        #here if point is out of grid, then return last grid coordinate
+        x_center = gx + (resolution / 2)
+        y_center = gy + (resolution / 2)
+        if (x_center > x_boundary):
+            x_center = x_boundary
+        if (y_center > y_boundary):
+            y_center = y_boundary
+        return (y_center, y_center)
+    
+    else:
+        return None
 
 if __name__ == '__main__':
     origin_x = 0
@@ -24,13 +38,20 @@ if __name__ == '__main__':
     height = 100
     resolution = 10 
 
+    print(f"Input: Start {(origin_x, origin_y)}, Max {(origin_x + width, origin_y + height)}, Resolution {resolution}")
+
     # Test the function with some example points
     points = [(10, 10), (55, 70), (-5, 20), (120, 80)]
     for point in points:
         grid_coords = world_to_grid(point[0], point[1], origin_x, origin_y, width, height, resolution)
+        world_coords = grid_to_world(point[0], point[1], origin_x, origin_y, width, height, resolution)
         if grid_coords:
-            print(f"Point {point} is in grid cell {grid_coords}")
+            print(f"World to Grid: Point {point} is in grid cell {grid_coords}")
         else:
-            print(f"Point {point} is outside the grid")
+            print(f"World to Grid: Point {point} is outside the grid")
+        if world_coords:
+            print(f"Grid to World: Point {point} is in grid cell {world_coords}")
+        else:
+            print(f"Grid to World: Point {point} is outside the grid")
 
     
